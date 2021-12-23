bb19d18da61047682e92a75bc2c859616355e3d7
BeardedPlatypus <mwtegelaers@gmail.com>
2021-12-23 14:52:24 +0100

    feat: Add github link to footer

--- ui/flowfm_inspector_gatsby/src/components/footer.js
+++ ui/flowfm_inspector_gatsby/src/components/footer.js
@@ -1,7 +1,21 @@
 import * as React from "react"
-import "../styles/global.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons"
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
 
+import "../styles/global.scss"
 import * as styles from "./footer.module.scss"
+
+function SocialButton(props) {
+  return (
+    <a href={props.link} target="_blank">
+      <span className="fa-layers fa-fw fa-2x has-text-white">
+        <FontAwesomeIcon icon={props.icon} transform="shrink-5" />
+      </span>
+    </a>
+  )
+}
+
 
 const Footer = () => (
   <footer className={`footer ${styles.stickyFooter} ${styles.textFooter} has-background-info has-text-white`}>
@@ -13,7 +27,7 @@ const Footer = () => (
       </div>
       <div className="level-right">
         <div className="level_item">
-          Github
+          <SocialButton link="https://github.com/BeardedPlatypus/FlowFM-Inspector" icon={faGithubAlt} />
         </div>
 
       </div>
