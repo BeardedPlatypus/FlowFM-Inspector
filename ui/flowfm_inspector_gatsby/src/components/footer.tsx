6bd860a54d880825538e2ad8a26155dc66d96423
BeardedPlatypus <mwtegelaers@gmail.com>
2022-01-03 07:11:35 +0100

    Update footer.js to footer.tsx

--- ui/flowfm_inspector_gatsby/src/components/footer.tsx
+++ ui/flowfm_inspector_gatsby/src/components/footer.tsx
@@ -1,11 +1,16 @@
 import * as React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
+import { faGithubAlt, IconDefinition } from "@fortawesome/free-brands-svg-icons";
 
 import "../styles/global.scss"
 import * as styles from "./footer.module.scss"
 
-function SocialButton(props) {
+interface SocialButtonProps {
+  link: string
+  icon: IconDefinition
+}
+
+const SocialButton: React.FC<SocialButtonProps> = (props: SocialButtonProps) => {
   return (
     <a href={props.link} target="_blank" rel="noreferrer">
       <span className="fa-layers fa-fw fa-2x has-text-white">
@@ -15,8 +20,7 @@ function SocialButton(props) {
   )
 }
 
-
-const Footer = () => (
+const Footer: React.FC = () => (
   <footer className={`footer ${styles.stickyFooter} ${styles.textFooter} has-background-info has-text-white`}>
     <div className="level">
       <div className="level-left">
