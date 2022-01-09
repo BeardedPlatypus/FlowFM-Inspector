7aae9ccbe0d054fac592ebf644f36e1b098b8f9f
BeardedPlatypus <mwtegelaers@gmail.com>
2022-01-09 19:43:51 +0100

    Add skeleton Main.fs

--- app/FlowFM-Inspector.Presentation.Main/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace FlowFM_Inspector.Presentation.Main
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ app/FlowFM-Inspector.Presentation/Main.fs
@@ -0,0 +1,22 @@
+﻿namespace FlowFM_Inspector.Presentation
+
+open Elmish.WPF
+
+module public Main =
+    type public Model = { todo : bool
+    }
+
+    [<RequireQualifiedAccess>]
+    type public CmdMsg =
+        | Initialise
+
+    type public Msg =
+        | NoOp
+
+    let public init (): Model * CmdMsg list = { todo = true }, [ CmdMsg.Initialise ]
+
+    let public update (msg: Msg) (model: Model) : Model * CmdMsg list =
+        match msg with 
+        | NoOp -> model, []
+
+    let bindings () : Binding<Model, Msg> list = []
