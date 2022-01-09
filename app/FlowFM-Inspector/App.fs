48a94d9277546cd06b8e418322d3f0bfbde765f5
BeardedPlatypus <mwtegelaers@gmail.com>
2022-01-09 19:44:12 +0100

    Configure App.fs

--- app/FlowFM-Inspector/App.fs
+++ app/FlowFM-Inspector/App.fs
@@ -1,4 +1,14 @@
-﻿
+﻿open System
+open Elmish.WPF
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open FlowFM_Inspector.Presentation.Views
+
+[<EntryPoint; STAThread>]
+let main _ =
+    Program.mkProgramWpfWithCmdMsg
+        FlowFM_Inspector.Presentation.Main.init 
+        FlowFM_Inspector.Presentation.Main.update 
+        FlowFM_Inspector.Presentation.Main.bindings
+        FlowFM_Inspector.CmdMapping.toCmd
+    |> Program.runWindow (MainView())
+
---
+++ app/FlowFM-Inspector/CmdMapping.fs
@@ -0,0 +1,9 @@
+﻿namespace FlowFM_Inspector
+
+open Elmish
+
+module public CmdMapping =
+    open FlowFM_Inspector.Presentation.Main
+
+    let public toCmd (cmdMsg: CmdMsg) : Cmd<Msg> = Cmd.Empty
+
