3699e1442dfb082af7d3e9c86b063c0592b69526
BeardedPlatypus <mwtegelaers@gmail.com>
2022-01-09 22:30:50 +0100

    Add webview component and the logic to load the app

--- app/FlowFM-Inspector.Presentation.Views/MainView.xaml.cs
+++ app/FlowFM-Inspector.Presentation.Views/MainView.xaml.cs
@@ -1,12 +1,35 @@
-﻿using MahApps.Metro.Controls;
+﻿using System;
+using MahApps.Metro.Controls;
+using Microsoft.Web.WebView2.Core;
 
 namespace FlowFM_Inspector.Presentation.Views
 {
     public partial class MainView : MetroWindow
     {
+        private const string AppName = "flowfm-inspector";
+
         public MainView()
         {
             InitializeComponent();
+            WebView.CoreWebView2InitializationCompleted += 
+                OnWebViewOnCoreWebView2InitializationCompleted;
+            WebView.Source = new Uri($"https://{AppName}/app/index.html");
+        }
+
+        private void OnWebViewOnCoreWebView2InitializationCompleted(
+            object? sender,
+            CoreWebView2InitializationCompletedEventArgs e)
+        {
+            if (e.IsSuccess) LoadWebApp();
+        }
+
+
+        public void LoadWebApp()
+        {
+            WebView.CoreWebView2.SetVirtualHostNameToFolderMapping(
+                hostName: AppName,
+                folderPath: "",
+                accessKind: CoreWebView2HostResourceAccessKind.Allow);
         }
     }
 }
