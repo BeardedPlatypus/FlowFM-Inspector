eb200fc50e9750fa2040023aefb7caca85a83943
BeardedPlatypus <mwtegelaers@gmail.com>
2022-01-15 16:58:36 +0100

    Add an initial router for appdata

---
+++ core/flowfm_inspector/routers/appdata.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, status
+from pathlib import Path
+from flowfm_inspector.state import appdata
+from flowfm_inspector.basemodel import BaseModel
+
+
+router = APIRouter(prefix="/api/appdata", tags=["appdata"])
+
+
+@router.get("/recent-projects", tags=["appdata"])
+async def retrieve_recent_projects():
+    return appdata.recent_projects.dict()
+
+
+class RecentProjectPutBody(BaseModel):
+    path: Path
+
+
+@router.put(
+    "/recent-projects", tags=["appdata"], status_code=status.HTTP_204_NO_CONTENT
+)
+async def update_recent_project(body: RecentProjectPutBody):
+    appdata.update_recent_project(body.path)
