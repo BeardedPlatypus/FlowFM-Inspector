06fb5bfa3d8320c608f5df1c7a1dc22c583a7852
BeardedPlatypus <mwtegelaers@gmail.com>
2022-01-16 17:29:52 +0100

    Add test Paths object

--- core/tests/paths.py
+++ core/tests/paths.py
@@ -5,3 +5,11 @@ class Paths:
     @staticmethod
     def test_folder() -> Path:
         return Path(__file__).parent
+
+    @staticmethod
+    def test_data_folder() -> Path:
+        return Paths.test_folder() / "test-data"
+
+    @staticmethod
+    def temp_folder() -> Path:
+        return Paths.test_data_folder() / "temp"
