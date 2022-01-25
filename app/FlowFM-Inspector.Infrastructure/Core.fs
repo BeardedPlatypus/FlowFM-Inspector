ac2d2f5e6fa5e0e42678631f80c481a843755645
BeardedPlatypus <mwtegelaers@gmail.com>
2022-01-25 21:22:33 +0100

    Add initial implementation for the Core module

---
+++ app/FlowFM-Inspector.Infrastructure/Core.fs
@@ -0,0 +1,26 @@
+﻿namespace FlowFM_Inspector.Infrastructure
+
+
+open System.Diagnostics
+open FlowFM_Inspector.Infrastructure.Processes.ChildProcess
+
+
+/// <summary>
+/// <see cref="Core"/> defines the methods related to the Python core server.
+/// </summary>
+module public Core =
+
+    /// <summary>
+    /// Start the Core server at the specified <paramref name="portNumber"/>.
+    /// </summary>
+    /// <param name="portNumber>The port number on which the server runs.</param>
+    let public Start (portNumber: uint) : unit =
+        let mutable startInfo = ProcessStartInfo()
+        startInfo.CreateNoWindow <- false
+        startInfo.WindowStyle <- ProcessWindowStyle.Hidden
+        startInfo.UseShellExecute <- false
+        startInfo.FileName <- "./server/core.exe"
+        startInfo.Arguments <- $"{portNumber}"
+
+        let serverProcess = Process.Start(startInfo)
+        AddChildProcess(serverProcess)
--- app/FlowFM-Inspector.Infrastructure/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace FlowFM_Inspector.Infrastructure
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
