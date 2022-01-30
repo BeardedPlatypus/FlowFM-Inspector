758351ac21ec53415819f5830a4c575eb1fd332a
BeardedPlatypus <mwtegelaers@gmail.com>
2022-01-30 21:25:35 +0100

    Add tools.py

---
+++ tools/tools/tools.py
@@ -0,0 +1,44 @@
+import typer
+
+from pathlib import Path
+from typing import Literal
+
+
+app = typer.Typer()
+
+
+@app.command()
+def generate_directories_fragment(
+    base_src_folder: Path = typer.Argument(..., help="The source folder in which all binaries are located"), 
+    target_directory: Path = typer.Argument(Path.cwd(), help="The target directory to which the `Directory.wxs` is written")
+):
+    """
+    Generate the `Directories.wxs` fragment. 
+
+    The `Directories.wxs` fragment is generated in the `target_directory`. If no 
+    target_directory is specified it will be generated in the current working 
+    directory.
+    """
+    pass
+
+
+Component = Literal["app", "core", "ui"]
+
+@app.command()
+def generate_component_fragment(
+    base_src_folder: Path = typer.Argument(..., help="The source folder in which all binaries are located"), 
+    component: Component = typer.Argument(..., help="The component type for which to create the fragment."), 
+    target_directory: Path = typer.Argument(Path.cwd(), help="The target directory to which the `Directory.wxs` is written")
+):
+    """
+    Generate the `<component>.wxs` fragment file.
+
+    The `<component>.wxs` fragment is generated in the target_directory. If no 
+    target_directory is specified it will be generated in the current working 
+    directory.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    app()
