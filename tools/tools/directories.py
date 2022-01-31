9e262b90c0bc187a01a87237aabbab1db77f8625
BeardedPlatypus <mwtegelaers@gmail.com>
2022-01-31 21:07:02 +0100

    Add initial implementation of directories.py

--- tools/tools/directories.py
+++ tools/tools/directories.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
+from jinja2 import Environment, PackageLoader, select_autoescape
 from pathlib import Path
-from typing import Collection
+from typing import Collection, Literal
+
+from tools.jinja_environment import env
+
+
+directory_template_name: Literal["Directories.wxs.jinja"] = "Directories.wxs.jinja"
 
 
 @dataclass
@@ -10,8 +16,14 @@ class DirectoryDescription:
     children: Collection["DirectoryDescription"]
 
 
-def gather_directories(self, base_src_path: Path) -> Collection[DirectoryDescription]:
+def generate_directories_fragment(base_src_path: Path):
+    directories = _gather_directories(base_src_path)
+
+
+def _gather_directories(base_src_path: Path) -> Collection[DirectoryDescription]:
     # App Directories
-
-
     return []
+
+def _render(directories: Collection[DirectoryDescription]) -> str:
+    template = env.get_template(directory_template_name)
+    return template.render(directories=directories)
