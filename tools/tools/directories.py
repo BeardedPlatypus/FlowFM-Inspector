28a38d8ddfb00b54e43eec5e0451cb51dc634aa4
BeardedPlatypus <mwtegelaers@gmail.com>
2022-02-01 22:15:51 +0100

    Add directories.py implementation

--- tools/tools/directories.py
+++ tools/tools/directories.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
-from jinja2 import Environment, PackageLoader, select_autoescape
 from pathlib import Path
-from typing import Collection, Literal
+from typing import Collection, Iterable, Literal
 
 from tools.jinja_environment import env
 
@@ -12,18 +11,38 @@ directory_template_name: Literal["Directories.wxs.jinja"] = "Directories.wxs.jin
 @dataclass
 class DirectoryDescription:
     id: str
-    name: Path
+    name: str
     children: Collection["DirectoryDescription"]
 
 
-def generate_directories_fragment(base_src_path: Path):
+def generate_directories_fragment(base_src_path: Path, write_path: Path):
     directories = _gather_directories(base_src_path)
+    content = _render(directories)
+    _write(content, write_path)
 
 
 def _gather_directories(base_src_path: Path) -> Collection[DirectoryDescription]:
-    # App Directories
-    return []
+    subdirectories = _retrieve_subdirectories(base_src_path)
+    return [_gather_directories_recursive(p, "") for p in subdirectories]
+
+
+def _gather_directories_recursive(path: Path, parent_id: str) -> DirectoryDescription:
+    id = f"{parent_id}.{path.name}" if parent_id else path.name
+    subdirectories = _retrieve_subdirectories(path)
+    children = [_gather_directories_recursive(p, id) for p in subdirectories]
+
+    return DirectoryDescription(id=id, name=path.name, children=children)
+
+
+def _retrieve_subdirectories(p: Path) -> Iterable[Path]:
+    return (subdir for subdir in p.iterdir() if subdir.is_dir())
+
 
 def _render(directories: Collection[DirectoryDescription]) -> str:
     template = env.get_template(directory_template_name)
     return template.render(directories=directories)
+
+
+def _write(content: str, write_path: Path) -> None:
+    with write_path.open("w") as f:
+        f.write(content)
