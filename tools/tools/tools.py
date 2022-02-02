31a30b9f5620d72fa74dff17345b5b8d7640943f
BeardedPlatypus <mwtegelaers@gmail.com>
2022-02-02 08:02:10 +0100

    Add implementation tools.generate_directories_fragment

--- tools/tools/tools.py
+++ tools/tools/tools.py
@@ -3,38 +3,52 @@ import typer
 from pathlib import Path
 from typing import Literal
 
+import tools.directories as directories
+
 
 app = typer.Typer()
 
 
 @app.command()
 def generate_directories_fragment(
-    base_src_folder: Path = typer.Argument(..., help="The source folder in which all binaries are located"), 
-    target_directory: Path = typer.Argument(Path.cwd(), help="The target directory to which the `Directory.wxs` is written")
+    base_src_folder: Path = typer.Argument(
+        ..., help="The source folder in which all binaries are located"
+    ),
+    target_directory: Path = typer.Argument(
+        Path.cwd(), help="The target directory to which the `Directory.wxs` is written"
+    ),
 ):
     """
-    Generate the `Directories.wxs` fragment. 
+    Generate the `Directories.wxs` fragment.
 
-    The `Directories.wxs` fragment is generated in the `target_directory`. If no 
-    target_directory is specified it will be generated in the current working 
+    The `Directories.wxs` fragment is generated in the `target_directory`. If no
+    target_directory is specified it will be generated in the current working
     directory.
     """
-    pass
+    target_file_path = target_directory / "Directories.wxs"
+    directories.generate_directories_fragment(base_src_folder, target_file_path)
 
 
 Component = Literal["app", "core", "ui"]
 
+
 @app.command()
 def generate_component_fragment(
-    base_src_folder: Path = typer.Argument(..., help="The source folder in which all binaries are located"), 
-    component: Component = typer.Argument(..., help="The component type for which to create the fragment."), 
-    target_directory: Path = typer.Argument(Path.cwd(), help="The target directory to which the `Directory.wxs` is written")
+    base_src_folder: Path = typer.Argument(
+        ..., help="The source folder in which all binaries are located"
+    ),
+    component: Component = typer.Argument(
+        ..., help="The component type for which to create the fragment."
+    ),
+    target_directory: Path = typer.Argument(
+        Path.cwd(), help="The target directory to which the `Directory.wxs` is written"
+    ),
 ):
     """
     Generate the `<component>.wxs` fragment file.
 
-    The `<component>.wxs` fragment is generated in the target_directory. If no 
-    target_directory is specified it will be generated in the current working 
+    The `<component>.wxs` fragment is generated in the target_directory. If no
+    target_directory is specified it will be generated in the current working
     directory.
     """
     pass
