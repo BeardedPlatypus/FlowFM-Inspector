8339a959a7ccabe7181ff8b38130648808eafd67
BeardedPlatypus <mwtegelaers@gmail.com>
2022-02-06 21:07:53 +0100

    Add initial dataclasses to components.py

---
+++ tools/tools/components.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Collection, Literal, Protocol
+
+from tools.jinja_environment import env
+import tools.persistence as persistence
+
+
+ComponentType = Literal["component", "component_group_ref"]
+component_template_name: Literal["Component.wxs.jinja"] = "Component.wxs.jinja"
+
+
+class ComponentGroupElement(Protocol):
+    @property
+    def component_type(self) -> ComponentType:
+        raise NotImplemented
+
+
+@dataclass
+class ComponentElement:
+    name: str
+    source: str
+
+    @property
+    def component_type(self) -> ComponentType:
+        return "component"
+
+
+@dataclass
+class ComponentGroupRef:
+    id: str
+
+    @property
+    def component_type(self) -> ComponentType:
+        return "component"
+
+
+def _render(component_groups: Collection[ComponentGroupElement]) -> str:
+    template = env.get_template(component_template_name)
+    return template.render(component_groups=component_groups)
