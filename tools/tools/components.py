69caa8bacfecd0a74659f96b9f20de076725f790
BeardedPlatypus <mwtegelaers@gmail.com>
2022-02-23 20:26:55 +0100

    Implement generate_components_fragment

--- tools/tools/components.py
+++ tools/tools/components.py
@@ -7,9 +7,7 @@ from typing import (
     Literal,
     Optional,
     Protocol,
-    Sequence,
     Set,
-    Union,
 )
 
 from tools.directories import directory_prefix
@@ -49,9 +47,9 @@ class ComponentGroupRef:
 @dataclass
 class ComponentGroup:
     id: str
-    dir: str
+    dir: Optional[str]
 
-    children: Collection[ComponentGroupElement]
+    children: List[ComponentGroupElement]
 
 
 @dataclass
@@ -59,9 +57,9 @@ class FragmentGenerationConfiguration:
     base_src_path: Path
     write_path: Path
     component_group_prefix: str
+    included_paths: Iterable[Path]
     ignored_paths: Set[Path]
-    additional_component_group_refs: Sequence[ComponentGroupRef]
-    is_root: bool
+    additional_component_group_refs: List[ComponentGroupRef]
 
 
 def generate_components_fragment(config: FragmentGenerationConfiguration):
@@ -73,30 +71,69 @@ def generate_components_fragment(config: FragmentGenerationConfiguration):
 def _gather_component_groups(
     config: FragmentGenerationConfiguration,
 ) -> Collection[ComponentGroup]:
-    # Each folder corresponds with a separate component group.
-    # The root component group
-    return []
+    results = [
+        _gather_component_groups_recursive(path, "", config)
+        for p in config.included_paths
+        if (path := config.base_src_path / p).exists() and path.is_dir()
+    ]
+
+    if (n_results := len(results)) == 0:
+        return []
+    elif n_results == 1:
+        res = results[0]
+        res.next_node.children += config.additional_component_group_refs
+
+        return [res.next_node] + res.deeper_nodes
+    else:
+        references = [ComponentGroupRef(id=cg.next_node.id) for cg in results]
+        references += config.additional_component_group_refs
+
+        children = references + config.additional_component_group_refs
+
+        top_group = ComponentGroup(
+            id=f"{config.component_group_prefix}-groups", dir=None, children=children
+        )
+
+        nodes = [cg for gr in results for cg in [gr.next_node] + gr.deeper_nodes]
+
+        return [top_group] + nodes
+
+
+@dataclass
+class GatherResult:
+    next_node: ComponentGroup
+    deeper_nodes: List[ComponentGroup]
 
 
 def _gather_component_groups_recursive(
     path: Path, parent_id: str, config: FragmentGenerationConfiguration
-) -> Collection[ComponentGroup]:
-    # Note that the ids of the component groups correspond with the folder and only differ in prefix
-    id = f"{parent_id}.{path.name}"
+) -> GatherResult:
+    # Note that the ids of the component groups correspond with the folder and only
+    # differ in prefix
+    if path == config.base_src_path:
+        # We set the id separately in the directory and component_group ids
+        id = ""
+    elif parent_id == "":
+        id = path.name
+    else:
+        id = f"{parent_id}.{path.name}"
 
-    directory_id = f"{directory_prefix}{id}"
-    component_group_id = f"{config.component_group_prefix}{id}"
+    directory_id = (
+        f"{directory_prefix}{id}" if path != config.base_src_path else "INSTALLFOLDER"
+    )
+
+    group_id_postfix = id if id != "" else "root"
+    component_group_id = f"{config.component_group_prefix}::{group_id_postfix}"
 
     child_components = _gather_components(path, config)
 
-    child_component_groups: List[ComponentGroup] = [
-        component_group
+    child_component_groups: List[GatherResult] = [
+        _gather_component_groups_recursive(p, id, config)
         for p in _get_directories(path, config)
-        for component_group in _gather_component_groups_recursive(p, id, config)
     ]
 
     child_component_group_references = [
-        ComponentGroupRef(id=cg.id) for cg in child_component_groups
+        ComponentGroupRef(id=cg.next_node.id) for cg in child_component_groups
     ]
 
     children = child_components + child_component_group_references  # type: ignore
@@ -105,7 +142,11 @@ def _gather_component_groups_recursive(
         id=component_group_id, dir=directory_id, children=children
     )
 
-    return [component_group] + child_component_groups
+    deeper_nodes = [
+        cg for gr in child_component_groups for cg in [gr.next_node] + gr.deeper_nodes
+    ]
+
+    return GatherResult(next_node=component_group, deeper_nodes=deeper_nodes)
 
 
 def _gather_components(
@@ -122,7 +163,7 @@ def _get_filepaths(
     path: Path, config: FragmentGenerationConfiguration
 ) -> Iterable[Path]:
     return (
-        p
+        p.relative_to(config.base_src_path)
         for p in path.glob("*")
         if p.is_file() and p.absolute() not in config.ignored_paths
     )
